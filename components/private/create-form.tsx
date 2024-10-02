93bc7b6ac12a60234b3866198b0f46e35901629b
Renato Filipi <re.rosa98@gmail.com>
2024-10-02 17:59:53 -0300

    chore: design create form dialog

--- components/private/create-form.tsx
+++ components/private/create-form.tsx
@@ -1,10 +1,22 @@
 "use client";
 
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
 import { minWidth640 } from "@/helpers/constants";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { ReactNode, useState } from "react";
+import { useForm } from "react-hook-form";
 import { useMediaQuery } from "react-responsive";
+import { z } from "zod";
+import { Button } from "../ui/button";
 import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
 import { Drawer, DrawerContent, DrawerTrigger } from "../ui/drawer";
+import { Input } from "../ui/input";
 
 const CreateForm = ({ children }: { children: ReactNode }) => {
   const isDesktop = useMediaQuery({ query: minWidth640 });
@@ -14,7 +26,7 @@ const CreateForm = ({ children }: { children: ReactNode }) => {
     return (
       <Dialog open={open} onOpenChange={setOpen}>
         <DialogTrigger asChild>{children}</DialogTrigger>
-        <DialogContent className="p-10">
+        <DialogContent className="">
           <Body />
         </DialogContent>
       </Dialog>
@@ -24,7 +36,7 @@ const CreateForm = ({ children }: { children: ReactNode }) => {
   return (
     <Drawer open={open} onOpenChange={setOpen}>
       <DrawerTrigger asChild>{children}</DrawerTrigger>
-      <DrawerContent className="p-10">
+      <DrawerContent className="p-6">
         <Body />
       </DrawerContent>
     </Drawer>
@@ -32,7 +44,49 @@ const CreateForm = ({ children }: { children: ReactNode }) => {
 };
 
 const Body = () => {
-  return <div className="p-10 flex justify-center items-center">Debug</div>;
+  const formSchema = z.object({ name: z.string() });
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-xl font-semibold">Create form</h1>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-6">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input type="text" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <div className="flex justify-end items-center">
+            <Button
+              type="submit"
+              variant={"secondary"}
+              size={"sm"}
+              className="w-full sm:w-fit">
+              Create form
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
 };
 
 export default CreateForm;
