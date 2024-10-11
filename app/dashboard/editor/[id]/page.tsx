d3a6554a397a311722c57af318d03bcb09a5cf6a
Renato Filipi <re.rosa98@gmail.com>
2024-10-11 12:17:31 -0300

    cp

--- app/dashboard/editor/[id]/page.tsx
+++ app/dashboard/editor/[id]/page.tsx
@@ -1,11 +1,19 @@
+"use client";
+
 import Brand from "@/components/core/brand";
 import { Button } from "@/components/ui/button";
+import { formList } from "@/mocks/forms";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Editor = () => {
+  const pathname = usePathname();
+  const currentFormId = pathname.split("/")[3];
+  const currentForm = formList.find((x) => x.id === currentFormId);
+
   return (
     <div className="flex flex-col w-full h-full">
-      <div className="flex justify-between items-center h-16 px-2 sm:px-6 z-10 bg-background border-b">
+      <div className="flex justify-between items-center h-16 px-2 sm:px-6 z-10 bg-background">
         <div className="flex justify-center items-center gap-4">
           <Button variant={"ghost"} size={"icon"} className="h-9 w-9" asChild>
             <Link href={"/dashboard/forms"}>
@@ -13,7 +21,7 @@ const Editor = () => {
             </Link>
           </Button>
           <span className="text-foreground/80 text-sm hidden sm:flex ">
-            Form name placeholder
+            {currentForm?.title}
           </span>
         </div>
         <div className="flex justify-center items-center gap-4">
