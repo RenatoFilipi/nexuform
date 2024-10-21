0f57d88a7ba7b44654f1dbb904aa7b0ef2f949a2
Renato Filipi <re.rosa98@gmail.com>
2024-10-21 09:16:40 -0300

    cp

--- components/private/forms/blocks/long-answer-block.tsx
+++ components/private/forms/blocks/long-answer-block.tsx
@@ -36,19 +36,47 @@ const LongAnswerBlock = ({
         </div>
         <div className="grid gap-1.5">
           <Label htmlFor="description">Description</Label>
-          <Textarea id="description" />
+          <Textarea
+            id="description"
+            value={block.description ?? ""}
+            onChange={(e) => {
+              updateBlock(id, { ...block, description: e.target.value });
+            }}
+          />
         </div>
         <div className="grid gap-1.5">
           <Label htmlFor="max-character-limit">Max character limit</Label>
-          <Input type="number" id="max-character-limit" />
+          <Input
+            type="number"
+            id="max-character-limit"
+            value={block.max_character_limit ?? 0}
+            onChange={(e) =>
+              updateBlock(id, {
+                ...block,
+                max_character_limit: Number(e.target.value),
+              })
+            }
+          />
         </div>
         <div className="flex justify-between items-center">
           <Label htmlFor="show-character-limit">Show character limit</Label>
-          <Switch id="show-character-limit" />
+          <Switch
+            id="show-character-limit"
+            checked={block.show_character_limit ?? false}
+            onCheckedChange={(checked: boolean) => {
+              updateBlock(id, { ...block, show_character_limit: checked });
+            }}
+          />
         </div>
         <div className="flex justify-between items-center">
           <Label htmlFor="required">Required</Label>
-          <Switch id="required" />
+          <Switch
+            id="required"
+            checked={block.required}
+            onCheckedChange={(checked: boolean) => {
+              updateBlock(id, { ...block, required: checked });
+            }}
+          />
         </div>
       </div>
       <div className="flex justify-end items-center">
