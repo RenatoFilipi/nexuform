aa5dc6f98c4d276efb73620e51dc36e417b1d753
Renato Filipi <re.rosa98@gmail.com>
2024-10-30 18:12:13 -0300

    chore: cp

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Brand from "@/components/core/brand";
-import { ModeToggle } from "@/components/core/mode-toggle";
-import { BorderBeam } from "@/components/magicui/border-beam";
+import ModeToggle2 from "@/components/core/mode-toggle2";
+import ShineBorder from "@/components/magicui/shine-border";
 import Nav from "@/components/public/nav";
 import { Button } from "@/components/ui/button";
 import { ArrowRightIcon, Menu } from "lucide-react";
@@ -10,7 +10,7 @@ import Link from "next/link";
 const Home = () => {
   return (
     <div className="min-h-screen flex flex-col relative">
-      <div className="flex fixed top-0 w-full sm:justify-around justify-between px-3 h-14 bg-background/80 z-10 backdrop-blur-md items-center">
+      <div className="flex fixed top-0 w-full justify-between sm:px-5 px-3 h-14 bg-background/80 z-10 backdrop-blur-md items-center">
         <Link href={"/"} className="flex justify-center items-center">
           <Brand type="logo_text" className="h-7 fill-foreground" />
         </Link>
@@ -30,7 +30,7 @@ const Home = () => {
           </Nav>
         </div>
       </div>
-      <div className="mt-20 relative flex flex-col justify-center items-center px-4 sm:px-0 flex-1 gap-14">
+      <div className="sm:mt-36 mt-20 relative flex flex-col justify-center items-center px-4 sm:px-0 flex-1 gap-14">
         <div className="flex justify-center items-center flex-col gap-6 w-full">
           <div className="flex flex-col justify-center items-center w-full text-center gap-6">
             <h1 className="font-bold text-3xl sm:text-6xl max-w-[43.5rem]">
@@ -55,29 +55,30 @@ const Home = () => {
           </div>
         </div>
         <div className="mb-20">
-          <div className="relative rounded-lg border p-2">
-            <BorderBeam borderWidth={3} colorTo="#75BDFF" colorFrom="#FFA775" />
+          <ShineBorder
+            className="relative rounded-lg border"
+            color={["#75BDFF", "#FF75E9", "#FFA775"]}>
             <Image
               alt="hero preview feature"
               src={"/dash.webp"}
-              width={1200}
+              width={1000}
               height={500}
             />
-          </div>
+          </ShineBorder>
         </div>
-        <footer className="flex justify-center items-center w-full h-12 absolute bottom-0 border-t">
-          <span className="text-sm text-foreground/80 sm:text-center">
-            © 2023{" "}
-            <Link href="/" className="hover:underline">
-              Nebulaform
-            </Link>
-            . All Rights Reserved.
-          </span>
-          <div className="absolute right-4 sm:flex hidden">
-            <ModeToggle />
-          </div>
-        </footer>
       </div>
+      <footer className="flex justify-center items-center w-full h-12 absolute bottom-0 border-t">
+        <span className="text-xs text-foreground/80 sm:text-center">
+          © 2023{" "}
+          <Link href="/" className="hover:underline">
+            Nebulaform
+          </Link>
+          . All Rights Reserved.
+        </span>
+        <div className="absolute right-4 hidden sm:flex">
+          <ModeToggle2 />
+        </div>
+      </footer>
     </div>
   );
 };
---
+++ components/core/mode-toggle2.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { MonitorIcon, MoonIcon, SunIcon } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useState } from "react";
+import { Label } from "../ui/label";
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
+
+const ModeToggle2 = () => {
+  const { setTheme, theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useQuery({
+    queryKey: [],
+    queryFn: () => {
+      setMounted(true);
+      return null;
+    },
+  });
+
+  if (!mounted) return null;
+
+  return (
+    <RadioGroup value={theme} onValueChange={setTheme} className="flex gap-1">
+      <div>
+        <RadioGroupItem value="system" id="system" className="peer sr-only" />
+        <Label
+          htmlFor="system"
+          className="text-xs cursor-pointer flex items-center justify-start gap-2 rounded-md border-1 border-muted bg-popover p-2 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:bg-foreground/10">
+          <MonitorIcon className="w-3 h-3" />
+        </Label>
+      </div>
+      <div>
+        <RadioGroupItem value="light" id="light" className="peer sr-only" />
+        <Label
+          htmlFor="light"
+          className="text-xs cursor-pointer flex items-center justify-start gap-2 rounded-md border-1 border-muted bg-popover p-2 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:bg-foreground/10">
+          <SunIcon className="w-3 h-3" />
+        </Label>
+      </div>
+      <div>
+        <RadioGroupItem value="dark" id="dark" className="peer sr-only" />
+        <Label
+          htmlFor="dark"
+          className="text-xs cursor-pointer flex items-center justify-start gap-2 rounded-md border-1 border-muted bg-popover p-2 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:bg-foreground/10">
+          <MoonIcon className="w-3 h-3" />
+        </Label>
+      </div>
+    </RadioGroup>
+  );
+};
+
+export default ModeToggle2;
