ba925ce91777585fea35d6712dbd73cf6e6f4f47
Renato Filipi <re.rosa98@gmail.com>
2024-11-07 15:13:05 -0300

    chore: cp

---
+++ mocks/analytics.ts
@@ -0,0 +1,18 @@
+import { AnalyticsProps } from "@/models/analytics";
+
+export const AnalyticsContent: AnalyticsProps = {
+  forms_total: 0,
+  submissions_total: 0,
+  forms: [
+    {
+      id: "01b1c3c1-0993-4ba5-a53f-604b7708f6e4",
+      name: "Product Evaluation",
+      submissions: 474,
+    },
+    {
+      id: "d9dd8f26-dabc-4f4c-bac3-facb19d621a8",
+      name: "Course Feedback",
+      submissions: 317,
+    },
+  ],
+};
--- models/analytics.ts
+++ models/analytics.ts
@@ -1,1 +1,11 @@
-export interface AnalyticsProps {}
+export interface AnalyticsProps {
+  forms_total: number;
+  submissions_total: number;
+  forms: FormProgressProps[];
+}
+
+export interface FormProgressProps {
+  id: string;
+  name: string;
+  submissions: number;
+}
