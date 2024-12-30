329bb603b499c2d677193a43468c5784014777fc
Renato Filipi <re.rosa98@gmail.com>
2024-12-30 13:29:14 -0300

    cp

--- components/public/landing/hero.tsx
+++ components/public/landing/hero.tsx
@@ -18,7 +18,7 @@ const Hero = () => {
               "group rounded-full border border-black/5 bg-neutral-100 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800"
             )}>
             <AnimatedShinyText className="text-xs inline-flex items-center justify-center px-4 py-1 transition ease-out text-foreground">
-              <span>🚧 Under Development</span>
+              <span>🔔 V1 Launch soon!</span>
               <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
             </AnimatedShinyText>
           </div>
