e1237f915d18159bdcdbfcb63649df103aeaa285
Renato Filipi <re.rosa98@gmail.com>
2025-01-13 13:27:36 -0300

    cp

--- components/private/editor/editor-preview-group.tsx
+++ components/private/editor/editor-preview-group.tsx
@@ -1,3 +1,5 @@
+import Brand from "@/components/core/brand";
+import { Button } from "@/components/ui/button";
 import useEditorStore from "@/stores/editor";
 import { ColorProps } from "@/utils/interfaces";
 import CheckBoxesDesign from "../blocks/design/checkboxes-design";
@@ -103,9 +105,11 @@ const design: ColorProps[] = [
 
 const EditorPreviewGroup = () => {
   const { blocks, form, theme } = useEditorStore();
+  const currentColor =
+    design.find((x) => x.label === theme.primary_color) ?? design[0];
 
   return (
-    <div className="flex flex-col gap-6 w-full border rounded m-8 p-6">
+    <div className="flex flex-col gap-6 w-full border rounded m-8 p-6 sm:w-[650px] bg-background overflow-y-auto">
       <div className="flex flex-col gap-1">
         <h1 className="text-2xl font-bold">{form.name}</h1>
         <p className="text-sm text-foreground/80">{form.description}</p>
@@ -196,6 +200,21 @@ const EditorPreviewGroup = () => {
               );
           }
         })}
+      </div>
+      <div className="flex flex-col gap-4">
+        <div className="flex justify-end items-center w-full">
+          <Button size={"sm"} className={`${currentColor.tw_class}`}>
+            {form.submit_text}
+          </Button>
+        </div>
+        <div className="flex justify-center items-center w-full">
+          <span className="border rounded p-2 w-fit flex justify-center items-center gap-2 hover:bg-foreground/5 cursor-pointer">
+            <Brand type="logo" className="fill-foreground w-4 h-4" />
+            <span className="text-foreground/80 text-sm font-semibold">
+              Powered by Nebulaform
+            </span>
+          </span>
+        </div>
       </div>
     </div>
   );
--- components/private/editor/editor-preview.tsx
+++ components/private/editor/editor-preview.tsx
@@ -4,8 +4,8 @@ import EditorPreviewGroup from "./editor-preview-group";
 
 const EditorPreview = () => {
   return (
-    <div className="flex w-full flex-1 h-full">
-      <span className="flex w-full h-full">
+    <div className="flex w-full flex-1 h-full overflow-y-auto">
+      <span className="flex w-full h-full justify-center bg-foreground/5 overflow-y-auto">
         <EditorPreviewGroup />
       </span>
     </div>
