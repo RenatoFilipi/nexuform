6a8e48d212eecbc5cb76f1934a18c7bbaea589e9
Renato <re.rosa98@gmail.com>
2025-03-01 21:02:36 -0300

    chore: chore: submissions by form analytics

--- components/private/analytics/analytics-submissions-by-form-chart.tsx
+++ components/private/analytics/analytics-submissions-by-form-chart.tsx
@@ -1,13 +1,52 @@
+import { Progress } from "@/components/ui/progress";
 import useAnalyticsStore from "@/stores/analytics";
-import { ConstructionIcon } from "lucide-react";
+import { formatDecimal } from "@/utils/functions";
+import { useQuery } from "@tanstack/react-query";
 
 const AnalyticsSubmissionsByFormChart = () => {
   const { submissions, forms } = useAnalyticsStore();
 
+  const query = useQuery({
+    queryKey: ["analyticsSubmissionsData"],
+    queryFn: () => {
+      const formCountMap: Record<string, number> = {};
+      for (const submission of submissions) {
+        formCountMap[submission.form_id] = (formCountMap[submission.form_id] || 0) + 1;
+      }
+      const subs = Object.entries(formCountMap).map(([form_id, count]) => ({
+        form_id,
+        name: forms.find((f) => f.id === form_id)?.name || form_id,
+        count,
+      }));
+
+      return { subs };
+    },
+    refetchOnWindowFocus: false,
+  });
+
+  if (query.isPending || !query.data) return null;
   return (
-    <div className="border flex justify-center items-center flex-col gap-2">
-      <ConstructionIcon className="w-6 h-6 text-primary" />
-      <span className="text-xs text-foreground/80">Under Development</span>
+    <div className="flex flex-col h-full gap-4">
+      <div className="flex justify-between items-center">
+        <span>Submissions by form</span>
+      </div>
+      <div className="flex flex-col gap-4">
+        {query.data.subs.map((sub) => {
+          const percentage = (100 * sub.count) / submissions.length;
+          return (
+            <div key={sub.form_id} className="flex flex-col gap-2 border p-4 rounded">
+              <div className="flex justify-between items-center">
+                <span className="text-sm">{sub.name}</span>
+                <div className="flex justify-center items-center gap-2">
+                  <span className="text-sm">{sub.count}</span>
+                  <span className="text-xs text-foreground/80">({formatDecimal(percentage, 2)}%)</span>
+                </div>
+              </div>
+              <Progress value={percentage} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -11,10 +11,10 @@ const Progress = React.forwardRef<
 >(({ className, value, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
-    className={cn("relative h-2 w-full overflow-hidden rounded bg-primary/10", className)}
+    className={cn("relative h-2 w-full overflow-hidden rounded bg-foreground/10", className)}
     {...props}>
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-secondary transition-all"
+      className="h-full w-full flex-1 bg-primary transition-all"
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
