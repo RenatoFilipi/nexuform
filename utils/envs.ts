bee768fd882788cb2e9595d2c469bc3e33ddda3f
Renato Filipi <re.rosa98@gmail.com>
2025-07-01 13:43:19 -0300

    chore: update env

--- utils/envs.ts
+++ utils/envs.ts
@@ -12,3 +12,32 @@ export const proForms = Number(process.env.NEXT_PUBLIC_PLAN_PRO_FORMS)!;
 export const proSubmissions = Number(process.env.NEXT_PUBLIC_PLAN_PRO_SUBMISSIONS)!;
 export const appName = process.env.NEXT_PUBLIC_APP_NAME!;
 export const contactEmail = process.env.NEXT_PUBLIC_CONTACT_EMAIL!;
+
+// V2
+
+// PLANS
+export const FREE_TRIAL_PERIOD = Number(process.env.NEXT_PUBLIC_FREE_TRIAL_PERIOD!);
+export const FREE_TRIAL_PRICE = Number(process.env.NEXT_PUBLIC_FREE_TRIAL_PRICE!);
+export const FREE_TRIAL_FORMS = Number(process.env.NEXT_PUBLIC_FREE_TRIAL_FORMS!);
+export const FREE_TRIAL_SUBMISSIONS = Number(process.env.NEXT_PUBLIC_FREE_TRIAL_SUBMISSIONS!);
+export const FREE_TRIAL_MAX_MEMBERS = Number(process.env.NEXT_PUBLIC_FREE_TRIAL_MAX_MEMBERS!);
+
+export const STARTER_PRICE = Number(process.env.NEXT_PUBLIC_STARTER_PRICE!);
+export const STARTER_FORMS = Number(process.env.NEXT_PUBLIC_STARTER_FORMS!);
+export const STARTER_SUBMISSIONS = Number(process.env.NEXT_PUBLIC_STARTER_SUBMISSIONS!);
+export const STARTER_MAX_MEMBERS = Number(process.env.NEXT_PUBLIC_STARTER_MAX_MEMBERS!);
+
+export const PRO_PRICE = Number(process.env.NEXT_PUBLIC_PRO_PRICE!);
+export const PRO_FORMS = Number(process.env.NEXT_PUBLIC_PRO_FORMS!);
+export const PRO_SUBMISSIONS = Number(process.env.NEXT_PUBLIC_PRO_SUBMISSIONS!);
+export const PRO_MAX_MEMBERS = Number(process.env.NEXT_PUBLIC_PRO_MAX_MEMBERS!);
+
+export const ADDITIONAL_MEMBER_PRICE = Number(process.env.NEXT_ADDITIONAL_MEMBER_PRICE!);
+
+// STRIPE
+export const STRIPE_PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!;
+
+// HELPERS
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME!;
+export const SUPPORT_EMAIL = process.env.NEXT_PUBLIC_SUPPORT_EMAIL!;
+export const CONTACT_EMAIL = process.env.NEXT_PUBLIC_CONTACT_EMAIL!;
